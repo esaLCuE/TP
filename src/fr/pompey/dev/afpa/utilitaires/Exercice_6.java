2a0d1bc36221c1ca069a4306177db4b136d76368
esaLCuE <louischenot@laposte.net>
2024-08-05 16:57:45 +0200

    Corrections

--- src/fr/pompey/dev/afpa/utilitaires/Exercice_6.java
+++ src/fr/pompey/dev/afpa/utilitaires/Exercice_6.java
@@ -1,8 +1,10 @@
 package fr.pompey.dev.afpa.utilitaires;
 
 
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.Scanner;
+
 
 public class Exercice_6 {
     public void exercice6(Scanner sc){
@@ -13,8 +15,10 @@ public class Exercice_6 {
             tabInt[i] = (int) (Math.random()*200-100);
 
         }
+        JOptionPane.showMessageDialog(null, Arrays.toString(tabInt));
         System.out.println(Arrays.toString(tabInt));
         TrierTableau.jsp(tabInt);
+        JOptionPane.showMessageDialog(null, Arrays.toString(tabInt));
         System.out.println(Arrays.toString(tabInt));
     }
 }
